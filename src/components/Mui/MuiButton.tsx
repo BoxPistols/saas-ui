68ee122d29444d97cffb761f37fb7fb692d0c63c
a.ito <10333049+BoxPistols@users.noreply.github.com>
2023-12-29 21:26:00 +0900

    Add Table Base

--- src/components/Mui/MuiButton.tsx
+++ src/components/Mui/MuiButton.tsx
@@ -1,20 +1,29 @@
-import React, { ReactNode } from "react"
-import { Button as MuiButton, ButtonProps as MuiButtonProps } from "@mui/material"
+import React, { ReactNode } from 'react'
+import {
+  Button as MuiButton,
+  ButtonProps as MuiButtonProps,
+} from '@mui/material'
 
-export interface ButtonProps extends Omit<MuiButtonProps, "size"> {
-  size?: "small" | "medium" | "large"
+export interface ButtonProps extends Omit<MuiButtonProps, 'size'> {
+  size?: 'small' | 'medium' | 'large'
   label?: string
-  variant?: "text" | "outlined" | "contained"
+  variant?: 'text' | 'outlined' | 'contained'
   endIcon?: ReactNode
   onClick?: () => void
 }
 
-export const Button = ({ size = "medium", label, variant, endIcon, ...props }: ButtonProps) => {
+export const Button = ({
+  size = 'medium',
+  label,
+  variant,
+  endIcon,
+  ...props
+}: ButtonProps) => {
   return (
     <>
       {/* <ThemeProvider theme={theme}> */}
       <MuiButton variant={variant} size={size} endIcon={endIcon} {...props}>
-        {label}
+        {label && <span>{label}</span>}
       </MuiButton>
     </>
   )
